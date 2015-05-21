7a93f2de145f02a107dffe1d1a1f1ee99ccf428c
mbastola <mbastola@wustl.edu>
2015-05-21 16:26:09 -0500

    Updated 5/21/15

--- main.sh
+++ main.sh
@@ -4,6 +4,7 @@ fileList="/etc/owping/pies.txt"
 sourceDir="/var/log/owlogs/"
 mkdir -m 777 -p $sourceDir
 ls $sourceDir > $fileList
+########Create Cron Job###############
 fileOut1="/etc/owping/owcron"
 if [ -e "$fileOut1" ];then
 	fileFlag=0
@@ -14,50 +15,62 @@ else
     chmod u+x $fileOut1
     echo $fileOut1 >> "/etc/rc.d/rc.local"
 fi
+##########Web Setup####################
+webDir="/var/www/html/owplot/"
+mkdir -p $webDir
+link1="/var/www/html/owplot/owping"
+link2="/var/www/html/owplot/js"
+if [ ! -L $link1 ]; then
+    ls -l "/etc/owping/" "owping"
+fi
+ 
+if [ ! -L $link2 ]; then
+    ls -l "/usr/local/share/gnuplot/5.0/js/" "js"
+fi
+mkdir -p "/var/www/html/owplot/index.html"
+#######################################
+
 while read line           
 do
-    cd /usr/local/bin/owping/
-    mkdir -p $line
+    mkdir -p "/etc/owping/$line"
     fileDate="/etc/owping/$line/outDate.txt"
     fileTime="/etc/owping/$line/outTime.txt"
     fileOpt="/etc/owping/$line/outOpt.txt"
     fileStat="/etc/owping/$line/lastupdated.txt"
     fileOut2="/etc/owping/$line/$line.js"
     fileOut3="/etc/owping/$line/plot.log"
-    fileFlag=1
-    if [ -e "$fileDate" ];then
-	fileFlag=0
-    else
+    fileOut4="/var/www/html/owplot/index.html/$line.php"
+    if [ ! -e "$fileOut3" ];then
+	(umask 000 ; touch $fileOut3)
+    fi
+    if [ ! -e "$fileDate" ];then
 	(umask 000 ; touch $fileDate)
 	echo "~" > $fileDate
     fi
-    if [ -e "$fileTime" ];then
-	fileFlag=0
-    else
+    if [ ! -e "$fileTime" ];then
       	(umask 000 ; touch $fileTime)
 	echo "~" > $fileTime
     fi
-    if [ -e "$fileOpt" ];then
-	fileFlag=0
-    else
+    if [ ! -e "$fileOpt" ];then
 	(umask 000 ; touch $fileOpt)
 	echo j > $fileOpt
     fi
-    if [ -e "$fileStat" ];then
-	fileFlag=0
-    else
+    if [ ! -e "$fileStat" ];then
 	(umask 000 ; touch $fileStat)
     fi
-    if [ -e "$fileOut2" ];then
-	fileFlag=0
-    else
+    if [ ! -e "$fileOut2" ];then
 	touch $fileOut2
-	STR=$'while (sleep 5 && [ /tmp/.${line}_lastupdate -nt /etc/owping/$line/lastupdated.txt ] || ( /etc/owping/main.sh >> $fileOut3 ; touch /tmp/.${line}_lastupdate )) &\ndo\n\twait \$!\ndone\n'
-	echo  $STR >> $fileOut1
+	echo "while (sleep 5 && [ /tmp/.${line}_lastupdate -nt /etc/owping/$line/lastupdated.txt ] || ( /etc/owping/main.sh >> $fileOut3 ; touch /tmp/.${line}_lastupdate )) &" >> $fileOut1
+	echo "do" >> $fileOut1
+	echo "wait \$!" >> $fileOut1
+	echo "done" >> $fileOut1
     fi
     date=$(cat "$fileDate")
     time=$(cat "$fileTime")
     opt=$(cat "$fileOpt")
     echo "$(/bin/sh /etc/owping/owplot.sh $line -t $time -d $date -x $opt)"
+    if [ ! -e "$fileOut4" ];then
+	echo "$(/bin/sh /etc/owping/owhtml.sh $line)"
+    fi
     done<$fileList
 rm $fileList
---
+++ owhtml.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+fileOutput="/var/www/html/owplot/index.html/$1.php"
+str="<html><head><meta charset=\"UTF-8\"><link type=\"text/css\" href=\"/owplot/js/gnuplot_mouse.css\" rel=\"stylesheet\"></head><body oncontextmenu=\"return false;\"><td align=\"center\"><script src=\"/owplot/js/canvastext.js\"></script><script src=\"/owplot/js/gnuplot_common.js\"></script><script src=\"/owplot/js/gnuplot_mouse.js\"></script><script src=\"/owplot/owping/$1/$1.js\"></script><table class=\"plot\"><tbody><tr><td><canvas id=\"gnuplot_canvas\" width=\"1000\" height=\"660\" onkeypress=\"gnuplot.do_hotkey()\" tabindex=\"0\"></canvas><script>gnuplot.grid_lines = true;gnuplot.zoomed = false;gnuplot.active_plot_name =\"gnuplot_canvas\";gnuplot.active_plot = function(){gnuplot_canvas();};if (window.attachEvent) {window.attachEvent('onload', gnuplot.active_plot);}else if (window.addEventListener) {window.addEventListener('load', gnuplot.active_plot, false);}else {document.addEventListener('load', gnuplot.active_plot, false);}</script></td></tr><tr><td><table id=\"gnuplot_mousebox\" class=\"mbunder\"><tbody><tr><td class=\"icon\" onclick=\"gnuplot.toggle_grid();\"><img src=\"/owplot/js/grid.png\" id=\"gnuplot_grid_icon\" alt=\"#\" title=\"toggle grid\"></td><td class=\"icon\" onclick=\"gnuplot.unzoom();\"><img src=\"/owplot/js/previouszoom.png\" id=\"gnuplot_unzoom_icon\" alt=\"unzoom\" title=\"unzoom\"></td>
+<td class=\"icon\" onclick=\"gnuplot.rezoom();\"><img src=\"/owplot/js/nextzoom.png\" id=\"gnuplot_rezoom_icon\" alt=\"rezoom\" title=\"rezoom\"></td><td class=\"icon\" onclick=\"gnuplot.toggle_plot(&quot;funcPlot_plot_1&quot;)\"></td>
+		    <td class=\"mb0\">x</td>
+		    <td class=\"mb1\"><span id=\"gnuplot_canvas_x\"></span></td>
+		    <td class=\"mb0\">y</td>
+		    <td class=\"mb1\"><span id=\"gnuplot_canvas_y\"></span></td>
+	      </tr></tbody></table>
+	       Options<br><br>
+	      <form action=\"\" method=\"POST\">
+		From date:<br>
+		<input type=\"text\" name=\"fromdate\" value="">
+		<br>
+		To date:<br>
+		<input type=\"text\" name=\"todate\" value="">
+		<br><br>
+		 
+		Start time:<br>
+		<input type=\"text\" name=\"starttime\" value="">
+		<br>End time:<br>
+		<input type=\"text\" name=\"endtime\" value="">
+		<br><br>
+
+		<input type=\"radio\" name=\"option\" value=\"l\"> Owping Loss<br>
+                <input type=\"radio\" name=\"option\" value=\"d\"> Owping Delay<br>
+		<input type=\"radio\" name=\"option\" value=\"j\"> Owping Jitter<br>
+		<br>
+		<input type=\"submit\" value=\"Submit\">
+	      </form>
+		 <?php
+		 \$fromdate = \$_POST[\"fromdate\"];
+                 \$todate = \$_POST[\"todate\"];
+                 \$parsedFromDate = date_parse_from_format(\"n-j-y\",\$fromdate);
+                 \$parsedToDate = date_parse_from_format(\"n-j-y\",\$todate);
+                 if (\$parsedFromDate[\"error_count\"] == 0 && checkdate(\$parsedFromDate[\"month\"], \$parsedFromDate[\"day\"], \$parsedFromDate[\"year\"]))
+		   \$checkedFromDate =  \$fromdate;  
+                 if(\$parsedToDate[\"error_count\"] == 0 && checkdate(\$parsedToDate[\"month\"], \$parsedToDate[\"day\"], \$parsedToDate[\"year\"]))
+		  \$checkedToDate =  \$todate;
+                 \$text = \"\$checkedFromDate~\$checkedToDate\n\";
+		 \$file = fopen(\"/etc/owping/$1/outDate.txt\",\"w\") or dieWithError(\"Unable to create file.\");
+		 fwrite(\$file, \$text);
+		 fclose(\$file);
+                 function checktime(\$hour, \$min) {
+		   if (\$hour < 0 || \$hour > 23 || !is_numeric(\$hour)) {
+		     return false;
+		   }
+		   if (\$min < 0 || \$min > 59 || !is_numeric(\$min)) {
+		     return false;
+		   }
+		   return true;
+		 }
+                 \$starttime = \$_POST[\"starttime\"];
+                 \$endtime = \$_POST[\"endtime\"];
+                 \$parsedStartTime = date_parse_from_format(\"h:i\",\$starttime);
+                 \$parsedEndTime = date_parse_from_format(\"h:i\",\$endtime); 
+                 if (\$parsedStartTime[\"error_count\"] == 0 && checktime(\$parsedStartTime[\"hour\"], \$parsedStartTime[\"minute\"]))
+		   \$checkedStartTime =  \$starttime;  
+                 if(\$parsedEndTime[\"error_count\"] == 0 && checktime(\$parsedEndTime[\"hour\"], \$parsedEndTime[\"minute\"]))
+		   \$checkedEndTime =  \$endtime;
+                 \$text2 =  \"\$checkedStartTime~\$checkedEndTime\n\";
+                 \$file2 = fopen(\"/etc/owping/$1/outTime.txt\",\"w\") or dieWithError(\"Unable to create file.\");
+		 fwrite(\$file2, \$text2);
+		 fclose(\$file2);
+                 \$opt = \$_POST[\"option\"];
+                 \$text3=\"\$opt\n\";                 
+                 \$file3= fopen(\"/etc/owping/$1/outOpt.txt\",\"w\") or dieWithError(\"Unable to create file.\");
+		 fwrite(\$file3, \$text3);
+		 fclose(\$file3);
+                 \$updatetime=date('m/d/Y h:i:s a');
+                 \$text4=\"\$updatetime\n\";
+                 \$file4= fopen(\"/etc/owping/$1/lastupdated.txt\",\"w\") or dieWithError(\"Unable to create file.\");
+		 fwrite(\$file4, \$text4);
+		 fclose(\$file4);
+                 sleep(10);
+		 ?> 
+		 <br>Note:
+                 <ul><li>(Date format  mm-dd-yy)
+                 </li><li>(Time format H:M)
+	      </li><li>Zoom using right (Firefox, Konqueror) or center (Opera, Safari) mouse button
+		</li><li>Mark point using left mouse button
+		</li><li>➀ toggles first plot on/off&nbsp;&nbsp;  # toggles grid on/off
+	      </li></ul>
+	  </td></tr>
+	</tbody>
+      </table>
+    </td>
+  </body>
+</html>
+"
+echo $str>$fileOutput
