600d5afe21a983cf81e8c79df4c70b04a5b2116d
Manil Bastola <mbastola@wustl.edu>
2015-05-29 16:34:50 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,18 +2,18 @@
 
 fileList="/etc/owping/pies.txt"
 sourceDir="/var/log/owlogs/"
+lastOne=""
 mkdir -m 777 -p $sourceDir
 ls $sourceDir > $fileList
-########Create Cron Job###############
-fileOut1="/etc/owping/owcron"
-if [ -e "$fileOut1" ];then
-	fileFlag=0
-else
-    (umask 000 ; touch $fileOut1)
-    echo "#!/bin/bash" > $fileOut1
-    echo "#This script run every 5 seconds" >> $fileOut1
-    chmod u+x $fileOut1
-    echo $fileOut1 >> "/etc/rc.d/rc.local"
+######################################
+fileStat="/etc/owping/lastupdated.txt"
+if [ ! -e "$fileStat" ];then
+    (umask 000 ; touch $fileStat)
+fi
+######################################
+filetk="/etc/owping/tk.txt"
+if [ ! -e "$filetk" ];then
+    (umask 000 ; touch $filetk)
 fi
 ##########Web Setup####################
 webDir="/var/www/html/owplot/"
@@ -23,20 +23,17 @@ link2="/var/www/html/owplot/js"
 if [ ! -L $link1 ]; then
     ls -l "/etc/owping/" "owping"
 fi
- 
 if [ ! -L $link2 ]; then
     ls -l "/usr/local/share/gnuplot/5.0/js/" "js"
 fi
 mkdir -p "/var/www/html/owplot/index.html"
-#######################################
-
+#########################################
 while read line           
 do
     mkdir -p "/etc/owping/$line"
     fileDate="/etc/owping/$line/outDate.txt"
     fileTime="/etc/owping/$line/outTime.txt"
     fileOpt="/etc/owping/$line/outOpt.txt"
-    fileStat="/etc/owping/$line/lastupdated.txt"
     fileOut2="/etc/owping/$line/$line.js"
     fileOut3="/etc/owping/$line/plot.log"
     fileOut4="/var/www/html/owplot/index.html/$line.php"
@@ -55,22 +52,13 @@ do
 	(umask 000 ; touch $fileOpt)
 	echo j > $fileOpt
     fi
-    if [ ! -e "$fileStat" ];then
-	(umask 000 ; touch $fileStat)
-    fi
     if [ ! -e "$fileOut2" ];then
 	touch $fileOut2
-	echo "while (sleep 5 && [ /tmp/.${line}_lastupdate -nt /etc/owping/$line/lastupdated.txt ] || ( /etc/owping/main.sh >> $fileOut3 ; touch /tmp/.${line}_lastupdate )) &" >> $fileOut1
-	echo "do" >> $fileOut1
-	echo "wait \$!" >> $fileOut1
-	echo "done" >> $fileOut1
     fi
-    date=$(cat "$fileDate")
-    time=$(cat "$fileTime")
-    opt=$(cat "$fileOpt")
-    echo "$(/bin/sh /etc/owping/owplot.sh $line -t $time -d $date -x $opt)"
     if [ ! -e "$fileOut4" ];then
-	echo "$(/bin/sh /etc/owping/owhtml.sh $line)"
+	/bin/sh /etc/owping/owhtml.sh $line
     fi
+    lastOne=$line
     done<$fileList
 rm $fileList
+echo $lastOne > $filetk
